c218da8812ed542a167529ac8c2804cded00e518
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-05-01 22:29:17 +0300

    [UPDATE] LanguageSerializer created, router created, LanguageView created

--- languages/urls.py
+++ languages/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
 
 app_name = 'languages'
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
